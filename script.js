2ddd4543c253a0545084d018738afbefb3285c76
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-07-01 22:26:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const fontSize = document.getElementById("fontsize").value;
+	const fontColor = document.getElementBydId("fontcolor").value;
+	document.cookie = `fontsize=${fontSize};`;
+	document.cookie = `fontcolor=${fontColor};`;
+	document.documentElement.style.setProperty("--fontsize", `${fontSize}px`);
+	document.documentElement.style.setProperty("--fontcolor", fontColor);
+})
